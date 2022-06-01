89d38e8877e34614d1f2e42bfe4a3e24fd4d193e
Rinyaresu <kaiquelinhares17@outlook.com>
2022-05-31 21:11:56 -0300

    feat: slice reducer

--- frontend/src/features/posts/postSlice.tsx
+++ frontend/src/features/posts/postSlice.tsx
@@ -1,3 +1,71 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import produce from "immer";
 import { RootState } from "../../app/store";
+
+export enum Statuses {
+  Initial = "Not Fetched",
+  Loading = "Loading...",
+  UpToDate = "Up to date",
+  Deleted = "Deleted",
+  Error = "Error",
+}
+
+export interface PostState {
+  id?: number;
+  title?: string;
+  body?: string;
+  created_at?: any;
+  updated_at?: any;
+}
+
+export interface PostsState {
+  posts: PostState[];
+  status: string;
+}
+
+const initialState: PostsState = {
+  posts: [
+    {
+      id: 0,
+      title: "",
+      body: "",
+      created_at: "",
+      updated_at: "",
+    },
+  ],
+  status: Statuses.Initial,
+};
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPostsAsync.pending, (state) => {
+        return produce(state, (draftState) => {
+          draftState.status = Statuses.Loading;
+        });
+      })
+
+      .addCase(fetchPostsAsync.fullfilled, (state) => {
+        return produce(state, (draftState) => {
+          draftState.status = Statuses.UpToDate;
+        });
+      })
+
+      .addCase(fetchPostsAsync.pending, (state) => {
+        return produce(state, (draftState) => {
+          draftState.status = Statuses.Error;
+        });
+      });
+  },
+});
+
+export const {} = postSlice.actions;
+
+export const selectPosts = (state: RootState) => state.posts.posts;
+
+export const selectStatus = (state: RootState) => state.posts.status;
+
+export default postSlice.reducer;
